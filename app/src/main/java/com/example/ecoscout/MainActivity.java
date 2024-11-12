9b8c8f051b061b7cd8d79ab2ca682f88793fe6d2
stephanie517 <125435148+stephanie517@users.noreply.github.com>
2024-11-12 22:25:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ecoscout/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.ecoscout;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
