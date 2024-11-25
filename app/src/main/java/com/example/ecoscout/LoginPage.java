b8d59e40dab59a41a0052a27c65d966724428093
faith <22-05861@g.batstate-u.edu.ph>
2024-11-25 22:06:25 +0800

    .

---
+++ app/src/main/java/com/example/ecoscout/LoginPage.java
@@ -0,0 +1,19 @@
+package com.example.ecoscout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+
+    public void Finish(View view) {
+        finish();
+    }
+}
