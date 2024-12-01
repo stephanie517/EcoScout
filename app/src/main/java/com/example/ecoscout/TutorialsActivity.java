407d90fa7d5a6c2e3e679ff89071bcca7f3c25f7
faith <22-05861@g.batstate-u.edu.ph>
2024-12-01 17:33:09 +0800

    ....

---
+++ app/src/main/java/com/example/ecoscout/TutorialsActivity.java
@@ -0,0 +1,15 @@
+package com.example.ecoscout;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TutorialsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorials);
+
+        // Add logic to display videos or interactive guides
+    }
+}
